ef71f66807a8facd71bc53eeeea70ac3e7e40244
annasavinskaya <savinskaya.ann@gmail.com>
2021-06-11 11:11:18 +0300

    add gifs and clear files

--- src/components/leadership/ContactUsBlock.vue
+++ src/components/leadership/ContactUsBlock.vue
@@ -2,55 +2,19 @@
   <div class="contact-block">
     <div class="contact-block--icon">
       <div class="contact-block--icon--desktop">
-        <vLottiePlayer
-          name="scrumlaunchLowerDesktopAnim"
-          loop
-          width="100%"
-          height="100%"
-          :animationData="require('../../assets/animation/scrumlaunch-lower-desktop.json')"
-        />
+        <img src="../../assets/animation/lower-block-desktop.gif" />
       </div>
       <div class="contact-block--icon--tablet">
-        <vLottiePlayer
-          name="scrumlaunchLowerTabletAnim"
-          loop
-          width="100%"
-          height="100%"
-          :animationData="require('../../assets/animation/scrumlaunch-lower-tablet.json')"
-        />
+        <img src="../../assets/animation/lower-block-tablet.gif" />
       </div>
       <div class="contact-block--icon--mobile">
-        <vLottiePlayer
-          name="scrumlaunchLowerMobileAnim"
-          loop
-          width="100%"
-          height="100%"
-          :animationData="require('../../assets/animation/scrumlaunch-lower-mobile.json')"
-        />
+        <img src="../../assets/animation/lower-block-mobile.gif" />
       </div>
     </div>
-    <!-- <inline-svg
-      :src="require('@/assets/icons/launch-mobile.svg')"
-      class="contact-block--icon icon-mobile"
-    />
-    <inline-svg
-      :src="require('@/assets/icons/launch-desktop.svg')"
-      class="contact-block--icon icon-desktop"
-    /> -->
     <p class="header-2">Looking to build something amazing? We’d love to help.</p>
     <router-link class="btn" :to="{ name: 'ContactUs'}">contact us</router-link>
   </div>
 </template>
-
-<script>
-import VueLottiePlayer from "vue-lottie-player";
-
-export default {
-  components: {
-    vLottiePlayer: VueLottiePlayer
-  },
-}
-</script>
 
 <style lang="scss" scoped>
 .contact-block {
@@ -97,7 +61,7 @@ export default {
 
     &--icon {
       top: -100px;
-      left: -49px;
+      left: -15px;
 
       &--desktop {
         display: none;
@@ -131,7 +95,7 @@ export default {
 
     &--icon {
       top: -150px;
-      left: -20px;
+      left: 0px;
 
       &--desktop {
         display: block;
