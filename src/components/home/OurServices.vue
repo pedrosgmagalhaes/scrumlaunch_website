c5200149605771df2e8d75d62a5881a61497853c
annasavinskaya <savinskaya.ann@gmail.com>
2021-07-08 10:37:23 +0300

    add pictures by hover services on home page

--- src/components/home/OurServices.vue
+++ src/components/home/OurServices.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="services">
     <h2 class="header-1 header-1-stroke">Our Services</h2>
-    <p class="header-1">Software Development</p>
-    <p class="header-1">DevOps</p>
-    <p class="header-1">UI/UX Design</p>
-    <p class="header-1">User Research & Product Analytics</p>
-    <p class="header-1">Brand Design</p>
-    <p class="header-1">Pitch Deck Creation</p>
-    <p class="header-1">Strategy</p>
+    <p class="header-1 software-text">Software Development</p>
+    <p class="header-1 devops-text">DevOps</p>
+    <p class="header-1 design-text">UI/UX Design</p>
+    <p class="header-1 product-text">User Research & <br class="brake"/>Product Analytics</p>
+    <p class="header-1 brand-text">Brand Design</p>
+    <p class="header-1 pitch-text">Pitch Deck Creation</p>
+    <p class="header-1 strategy-text">Strategy</p>
   </div>
 </template>
 
@@ -25,6 +25,10 @@ export default {
   margin: 80px 0;
   text-align: right;
 
+  .brake {
+    display: block;
+  }
+
   h2 {
     margin-bottom: 40px;
     -webkit-text-stroke: 0.5px #1E1F21;
@@ -35,13 +39,100 @@ export default {
     transition: all .4s ease;
     width: fit-content;
     font-size: 20px;
+    position: relative;
 
     &:hover {
       color: #12E2B0;
+
+      &::before {
+        opacity: 1;
+        visibility: visible;
+      }
     }
 
     &:last-child {
       margin-bottom: 0px;
+    }
+
+    &::before {
+      position: absolute;
+      content: '';
+      width: 121px;
+      height: 92px;
+      opacity: 0;
+      visibility: hidden;
+      transition: all .4s ease;
+      top: 50%;
+      transform: translateY(-50%);
+      pointer-events: none;
+      background-position: center;
+      background-size: contain;
+      z-index: 1;
+    }
+  }
+
+  .software-text {
+    &::before {
+      background-image: url('../../assets/images/services/sofrware.png');
+      left: -64px;
+    }
+  }
+
+  .devops-text {
+    &::before {
+      background-image: url('../../assets/images/services/devops.png');
+      left: -135px;
+    }
+  }
+
+  .design-text {
+    &::before {
+      background-image: url('../../assets/images/services/design.png');
+      left: -135px;
+    }
+  }
+
+  .product-text {
+    &::before {
+      background-image: url('../../assets/images/services/product.png');
+      left: -114px;
+    }
+  }
+
+  .brand-text {
+    &::before {
+      background-image: url('../../assets/images/services/brand.png');
+      left: -135px;
+    }
+  }
+
+  .pitch-text {
+    &::before {
+      background-image: url('../../assets/images/services/pitch.png');
+      left: -102px;
+    }
+  }
+
+  .strategy-text {
+    &::before {
+      background-image: url('../../assets/images/services/strategy.png');
+      left: -135px;
+    }
+  }
+}
+
+@media screen and (min-width: 414px) {
+  .services {
+    .software-text {
+      &::before {
+        left: -102px;
+      }
+    }
+
+    .product-text, .pitch-text {
+      &::before {
+        left: -135px;
+      }
     }
   }
 }
@@ -59,6 +150,44 @@ export default {
     p {
       margin-bottom: 36px;
       font-size: 40px;
+
+      &::before {
+        width: 213px;
+        height: 162px;
+      }
+    }
+
+    .software-text {
+      &::before {
+        left: -167px;
+      }
+    }
+
+    .devops-text,
+    .design-text,
+    .brand-text,
+    .product-text,
+    .pitch-text,
+    .strategy-text {
+      &::before {
+        left: -233px;
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .services {
+    .software-text,
+    .devops-text,
+    .design-text,
+    .brand-text,
+    .product-text,
+    .pitch-text,
+    .strategy-text {
+      &::before {
+        left: -233px;
+      }
     }
   }
 }
@@ -75,6 +204,58 @@ export default {
     p {
       margin-bottom: 52px;
       font-size: 56px;
+
+      &::before {
+        width: 363px;
+        height: 276px;
+      }
+    }
+
+    .software-text {
+      &::before {
+        left: -367px;
+      }
+    }
+
+    .devops-text,
+    .design-text,
+    .brand-text,
+    .product-text,
+    .pitch-text,
+    .strategy-text {
+      &::before {
+        left: -417px;
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 1440px) {
+  .services {
+    .brake {
+      display: none;
+    }
+
+    .software-text {
+      &::before {
+        left: -417px;
+      }
+    }
+
+    .product-text {
+      &::before {
+        left: -46px;
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 1920px) {
+  .services {
+    .product-text {
+      &::before {
+        left: -417px;
+      }
     }
   }
 }
