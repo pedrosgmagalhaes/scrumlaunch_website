30566deaf6557137d841085469a0a44a30111edd
pqoqubbw <iffweknow@gmail.com>
2022-06-28 18:32:50 +0200

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.API_URL },
+      { hid: 'description', name: 'description', content: "" },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
