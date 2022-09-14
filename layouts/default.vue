c50f6e363a4e2844b5a437de4a37fbddc1af0d49
pqoqubbw <iffweknow@gmail.com>
2022-09-14 19:28:02 +0200

    add pasted code for staging

--- layouts/default.vue
+++ layouts/default.vue
@@ -90,6 +90,36 @@ export default {
       document.body.appendChild(intercomSettings)
       document.body.appendChild(intercomScript)
       // intercom
+
+      // linkedin
+      const linkedinSettings = document.createElement('script')
+      linkedinSettings.innerHTML = `_linkedin_partner_id = "4702825";window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];window._linkedin_data_partner_ids.push(_linkedin_partner_id);`
+
+      const linkedinScript = document.createElement('script')
+      linkedinScript.innerHTML = `(function(l) {if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s = document.getElementsByTagName("script")[0];var b = document.createElement("script");b.type = "text/javascript";b.async = true;b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b, s);})(window.lintrk);;`
+
+      document.body.appendChild(linkedinSettings)
+      document.body.appendChild(linkedinScript)
+      // linkedin
+
+      // meta pixel
+      const metaPixelSettings = document.createElement('script')
+      metaPixelSettings.innerHTML = `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '411211710930789');fbq('track', 'PageView');`
+      document.body.appendChild(metaPixelSettings)
+      // meta pixel
+
+      // visitor tracker
+      const visitorTrackerSettings = document.createElement('script')
+      visitorTrackerSettings.setAttribute('async', "")
+      visitorTrackerSettings.setAttribute('src', "http://script.crazyegg.com/pages/scripts/0114/5321.js")
+      document.body.appendChild(visitorTrackerSettings)
+      // visitor tracker
+
+      // feeder
+      const feederSettings = document.createElement('script')
+      feederSettings.innerHTML = `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('ywVkO4XVpRe8Z6Bj');`
+      document.body.appendChild(feederSettings)
+      // feeder
     }
 
     if (window.location.hostname.includes('localhost')) {
