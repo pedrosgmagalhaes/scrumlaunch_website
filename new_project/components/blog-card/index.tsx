610b30edc532691bc424b69e5f3ffd1bbdd61b4c
Marcelo Oliveira <marcelo.oliveira@scrumlaunch.com>
2022-12-14 14:02:20 -0300

    finish secondary article on blog component

--- new_project/components/blog-card/index.tsx
+++ new_project/components/blog-card/index.tsx
@@ -2,7 +2,43 @@ import React from 'react'
 import Image from 'next/image'
 import style from './style.module.scss'
 import Blog from '../../public/assets/blog.svg'
+import Blog2 from '../../public/assets/blog2.svg'
+import Blog3 from '../../public/assets/blog3.svg'
+import Blog4 from '../../public/assets/blog4.svg'
 import BlogAvatar from '../../public/assets/blogAvatar.svg'
+
+const articleArray = [
+  {
+    image: Blog2,
+    tag: 'Design',
+    title: 'How design makes the world: Typos and corrections',
+    text: 'Whether you spend time in a studio or a boardroom, an office or the outdoors, How Design Makes the World empowers you to ask better questions—and to understand the designs in everything that matters.',
+    author: 'Thomas Jefferson',
+    articleDate: '22/09/2022',
+    authorAvatar: BlogAvatar,
+    tagColor: '#E2CCFF',
+  },
+  {
+    image: Blog3,
+    tag: 'Self-development',
+    title: 'How To Improve Your Problem-Solving Skills',
+    text: 'Whether you spend time in a studio or a boardroom, an office or the outdoors, How Design Makes the World empowers you to ask better questions—and to understand the designs in everything that matters.',
+    author: 'Thomas Jefferson',
+    articleDate: '22/09/2022',
+    authorAvatar: BlogAvatar,
+    tagColor: '#FFCCDC',
+  },
+  {
+    image: Blog4,
+    tag: 'Management',
+    title: 'How to Deal With a Lack of Communication At Work',
+    text: 'Whether you spend time in a studio or a boardroom, an office or the outdoors, How Design Makes the World empowers you to ask better questions—and to understand the designs in everything that matters.',
+    author: 'Thomas Jefferson',
+    articleDate: '22/09/2022',
+    authorAvatar: BlogAvatar,
+    tagColor: '#FFD7C2',
+  },
+]
 
 export default function BlogCard() {
   return (
@@ -44,7 +80,7 @@ export default function BlogCard() {
               matters.
             </p>
 
-            <h3>Read more</h3>
+            <h3 className={style.readMore}>Read more</h3>
 
             <div className={style.separator} />
 
@@ -59,6 +95,43 @@ export default function BlogCard() {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className={style.articleSecondaryCard}>
+        {articleArray.map((article) => (
+          <div key={Math.random()} className={style.singleArticle}>
+            <Image
+              className={style.articleSecondaryImg}
+              src={article.image}
+              alt="secondary blog avatar"
+            />
+
+            <span
+              className={style.articleTag1}
+              style={{ backgroundColor: article.tagColor }}
+            >
+              {article.tag}
+            </span>
+
+            <h1 className={style.articleTitle}>{article.title}</h1>
+
+            <p>{article.text}</p>
+
+            <h3 className={style.readMore}>Read more</h3>
+
+            <div className={style.separator} />
+
+            <div className={style.authorInfo}>
+              <Image src={article.authorAvatar} alt="author avatar" />
+
+              <div>
+                <h4>{article.author}</h4>
+
+                <h6>{article.articleDate}</h6>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
