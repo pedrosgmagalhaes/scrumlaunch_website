dd44a9a2db2c3a3404e1213f6f7cd979375457c4
Marcelo Oliveira <marcelo.oliveira@scrumlaunch.com>
2022-12-15 15:53:03 -0300

    initial carousel styling and logic

--- new_project/components/carousel-case-studies-card/index.tsx
+++ new_project/components/carousel-case-studies-card/index.tsx
@@ -1,39 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
 import Button from '../buttons/accept-button'
 import Arrow from '../../public/icons/arrowBlack.svg'
+import CarouselMobile1 from '../../public/assets/carouselCase1Mobile.svg'
+import CarouselDesktop1 from '../../public/assets/carouselCase1Desktop.svg'
 import style from './style.module.scss'
 
 export default function CarouselCaseCard() {
+  const [positionSelected, setPositionSelected] = useState(0)
+
+  const handleChangeSelector = (position: number) => {
+    setPositionSelected(position)
+  }
+
   return (
-    <div className={style.carouselCase}>
-      <div className={style.animationCase}>
-        <h2>Case studies.</h2>
+    <div className={style.carouselCaseComponent}>
+      <div className={style.carouselCase}>
+        <div className={style.animationCase}>
+          <h2>Case studies.</h2>
 
-        <div>
           <div>
-            <h3>Harvest</h3>
-
-            <p>
-              From an idea to a market leader with millions of users powered by
-              ScrumLaunch.
-            </p>
-
             <div>
-              <div />
+              <h3>Harvest</h3>
 
-              <div />
+              <p>
+                From an idea to a market leader with millions of users powered
+                by ScrumLaunch.
+              </p>
 
-              <div />
+              <div className={style.cards}>
+                <div>
+                  10M
+                  <span>Active users</span>
+                </div>
+
+                <div>
+                  $3M
+                  <span>Saved on Devlopment Costs</span>
+                </div>
+
+                <div>
+                  15 Dedicated team members
+                  <span />
+                </div>
+              </div>
+
+              <Button
+                buttonText="View case"
+                color="black"
+                onClickHandler={() => {}}
+                icon={Arrow}
+              />
             </div>
 
-            <Button
-              buttonText="View case"
-              color="black"
-              onClickHandler={() => {}}
-              icon={Arrow}
-            />
+            <Image src={CarouselMobile1} alt="mobile case 1" />
+
+            <Image src={CarouselDesktop1} alt="desktop case 1" />
           </div>
         </div>
+      </div>
+
+      <div className={style.carouselControl}>
+        {new Array(5).fill(1).map((_, index) => (
+          <div
+            key={Math.random()}
+            role="presentation"
+            className={style.selector}
+            style={{
+              backgroundColor:
+                positionSelected === index ? '#1E1F21' : '#f4f2ec',
+            }}
+            onClick={() => handleChangeSelector(index)}
+            onKeyDown={() => handleChangeSelector(index)}
+          />
+        ))}
       </div>
     </div>
   )
