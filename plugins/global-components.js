945a0ddd34fb613522d33d51a0d28ce8cb7af64a
Marcelo Oliveira <marcelo.oliveira@scrumlaunch.com>
2022-12-22 11:22:37 -0300

    clean repository

--- plugins/global-components.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-import AppearFromMask from '@/components/animations/AppearFromMask.vue'
-import TypingEffect from '@/components/animations/TypingEffect.vue'
-
-Vue.component('AppearFromMask', AppearFromMask)
-Vue.component('TypingEffect', TypingEffect)
