c41b46953de830b66957e6376906b190448b52ae
Rostislav Bereza <rostik@scrumlaunch.com>
2023-01-23 16:16:28 +0200

    feat: added technologies carousel

--- components/carousel-technologies/index.tsx
+++ components/carousel-technologies/index.tsx
@@ -6,6 +6,10 @@ import JavaScriptImg from '../../public/assets/technologies/js.jpg'
 import ReactImg from '../../public/assets/technologies/react.jpg'
 import NodeImg from '../../public/assets/technologies/node.jpg'
 import PythonImg from '../../public/assets/technologies/python.jpg'
+import FlutterImg from '../../public/assets/technologies/flutter.jpg'
+import PhpImg from '../../public/assets/technologies/php.jpg'
+import RubyImg from '../../public/assets/technologies/ruby.jpg'
+import SwiftImg from '../../public/assets/technologies/swift.jpg'
 
 import style from './style.module.scss'
 
@@ -29,6 +33,22 @@ const images = [
   {
     image: PythonImg,
     name: 'Python'
+  },
+  {
+    image: FlutterImg,
+    name: 'Flutter'
+  },
+  {
+    image: PhpImg,
+    name: 'PHP'
+  },
+  {
+    image: RubyImg,
+    name: 'Ruby on Rails'
+  },
+  {
+    image: SwiftImg,
+    name: 'Swift'
   },
 ]
 
